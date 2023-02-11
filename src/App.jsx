c5aa2bc45d80399a7c9ad77404e2696116f5c8b4
Zaryab <zaryabdev404@gmail.com>
2023-02-11 17:07:24 +0500

    added: new example

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import Example from "./example";
+// import Example from "./example";
+import Example from './example-ref/example';
 function App() {
     return (
         <DndProvider backend={HTML5Backend}>
---
+++ src/example-ref/Container.jsx
@@ -0,0 +1,9 @@
+import React, { memo, useCallback, useReducer, useState } from "react";
+
+export const Container = memo(function Container() {
+    return (
+        <div className="">
+            Hi
+        </div>
+    );
+});
