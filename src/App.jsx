f3e2d4bdf0ef707cb4e2fbd959759f85c42f8b8d
talhazaryab <talhazaryabmail@gmail.com>
2023-02-11 20:31:57 +0500

    added: example for ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import Example from "./example";
+// import Example from "./tabs-example/example";
+import Example from "./pics-example/example";
+import EaxmpleMe from "./pics-me-example/example";
 function App() {
     return (
-        <DndProvider backend={HTML5Backend}>
-            <Example />
-        </DndProvider>
+        <div className="App">
+            <DndProvider backend={HTML5Backend}>
+                <Example />
+            </DndProvider>
+            <hr />
+            <DndProvider backend={HTML5Backend}>
+                <EaxmpleMe />
+            </DndProvider>
+        </div>
     );
 }
 
---
+++ src/pics-me-example/Container.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useReducer } from "react";
+
+
+export function Container() {
+
+    return (
+        <main>
+            C
+        </main>
+    );
+}
